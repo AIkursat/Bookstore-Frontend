9919c2313e088a4c3dbdd28223617de824363485
AIKursat <aynurkursat1999@hotmail.com>
2022-07-04 17:07:43 +0300

    Editing main structure

--- vue-app/src/components/BodyPart.vue
+++ vue-app/src/components/BodyPart.vue
@@ -1,3 +1,9 @@
 <template>
-    <h2>fsafa</h2>
+    <div class="container">
+        <div class="classs">
+            <div class="col">
+                <img alt="Book and Glasses" src = "./../assets/book-and-glasses.png" class="img-fluid mt-5">
+            </div>
+        </div>
+    </div>
 </template>
