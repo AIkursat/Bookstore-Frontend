ee123680868430172e83f415a200524498f62c08
AIKursat <aynurkursat1999@hotmail.com>
2022-07-05 15:11:44 +0300

    Updating our navigation links

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import BodyPart from './../components/BodyPart'
+import BodyPart from './../components/BodyPart.vue'
+import Login from './../components/Login.vue'
 
 const routes = [
    {
     path: '/', // That's what you'd see in the browser location bar
     name: 'Home', // Because it's home page
     component: BodyPart,    
+   },
+   {
+    path: '/login',
+    name: 'Login',
+    component: Login,
    }
 ]
 
