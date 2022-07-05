e63bb5b2436b3e0f33c0be4155e4d2f406894800
AIKursat <aynurkursat1999@hotmail.com>
2022-07-05 14:50:56 +0300

    Adding routes

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <HeaderPart />
-  <BodyPart />
+
+  <div>
+    <router-view/>
+  </div>
   <FooterPart />
 </template>
 
 <script>
 import HeaderPart from "./components/HeaderPart.vue"
-import BodyPart from "./components/BodyPart.vue"
+
 import FooterPart from "./components/FooterPart.vue"
 export default {
   name: "App",
   components: {
     HeaderPart,
-    BodyPart,
     FooterPart,
   }
 }
--- vue-app/src/components/BodyPart.vue
+++ vue-app/src/components/BodyPart.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="classs">
             <div class="col">
-                <img alt="Book and Glasses" src = "./../assets/book-and-glasses.png" class="img-fluid mt-5">
+                <img alt="Book and Glasses" src= "./../assets/book-and-glasses.png" class="rounded mx-auto d-block">
             </div>
         </div>
     </div>
