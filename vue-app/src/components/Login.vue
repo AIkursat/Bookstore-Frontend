11a7e1a6af09e140de4de92b3da59138a8015dac
AIKursat <aynurkursat1999@hotmail.com>
2022-07-05 18:01:58 +0300

    Adding forms and add to login

--- vue-app/src/components/Login.vue
+++ vue-app/src/components/Login.vue
@@ -1,3 +1,61 @@
 <template>
-    <p>Login</p>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-5">Login</h1>
+                <hr>
+                <form method="post" action="/login" class="needs-validation" novalidate>
+
+                    <text-input
+                        label="Email"
+                        type="email"
+                        name="email"
+                        required="true">
+                    </text-input>
+
+                    <text-input
+                        label="Password"
+                        type="password"
+                        name="password"
+                        required="true">
+                    </text-input>
+
+                    <hr>
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import TextInput from './forms/TextInput.vue'
+
+export default {
+    name: 'login',
+    components: {
+        TextInput,
+    },
+    mounted() {
+        (function () {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+                })
+            })()
+    }
+}
+</script>
---
+++ vue-app/src/components/forms/SelectInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="mb-3">
+        <label :for="name" class="form-label">{{label}}</label>
+        <select :id="name" class="form-select" :name="name" :required="required">
+            <option v-for="option in items" :value="option.value" :key="option.value">
+                {{option.text}}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectInput',
+    props: ["items", "name", "required", "label"],
+}
+</script>
