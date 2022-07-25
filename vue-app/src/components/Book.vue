789ec8ce4a53ccdc947bbec0a46e91b29406a37b
AIKursat <aynurkursat1999@hotmail.com>
2022-07-25 23:00:44 +0300

    Create new routes

---
+++ vue-app/src/components/Book.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="row">
+        <h1 class="mt-3">Book</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Body from './../components/Body.vue'
 import Login from './../components/Login.vue'
+import Books from './../components/Books.vue'
+import Book from './../components/Book.vue'
+import BooksAdmin from './../components/BooksAdmin.vue'
+import BookEdit from './../components/BookEdit.vue'
+import Users from './../components/Users.vue'
+import User from '../components/UserEdit.vue'
 
 const routes = [
    {
@@ -12,7 +18,38 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+   },
+   {
+      path: '/books',
+      name: 'Books',
+      component: Books,
+   },
+   {
+      path: '/books/:bookName',
+      name: 'Book',
+      component: Book,
+   },
+   {
+      path: 'admin/books',
+      name: 'BooksAdmin',
+      component: BooksAdmin,
+   },
+   {
+      path: '/admin/books/:BookId',
+      name: 'BookEdit',
+      component: BookEdit,
+   },
+   {
+      path: '/admin/users',
+      name: 'Users',
+      component: Users,
+   },
+   {
+      path: '/admin/users/:userId',
+      name: 'User',
+      component: User,
    }
+
 ]
 
 const router = createRouter({history: createWebHistory(), routes})
