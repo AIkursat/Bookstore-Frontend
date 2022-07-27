c96b55ae5adc48365e762bce1e6b310bb4b63a44
AIKursat <aynurkursat1999@hotmail.com>
2022-07-27 21:17:55 +0300

    Get all users

--- vue-app/src/components/Users.vue
+++ vue-app/src/components/Users.vue
@@ -10,10 +10,35 @@
 
 <script>
 import Security from './security.js'
+import notie from 'notie'
 
 export default{
+  data() {
+    return{
+      users: [],
+    }
+  },
   beforeMount() {
     Security.requireToken();
+
+    fetch("http://localhost:8081/admin/users", Security.requestOptions(""))
+    .then((response) => response.json())
+    .then((response) =>{
+      if(response.error){
+         notie.alert({
+          type: "error:",
+          text: response.message,
+         })
+      }else{
+        this.users = response.data.users;
+      }
+    })
+    .catch((error) => {
+          notie.alert({
+          type: "error:",
+          text: error,
+         })
+    });
   },
 }
 
