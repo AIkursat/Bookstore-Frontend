f3a036a56935d47003c8d8a8ee4f384185810884
AIKursat <aynurkursat1999@hotmail.com>
2022-07-29 16:04:54 +0300

    Edit existing user

--- vue-app/src/components/UserEdit.vue
+++ vue-app/src/components/UserEdit.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="row">
-        <h1 class="mt-3">User</h1>
-         <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-3">User</h1>
+                <hr>
                 <form-tag @userEditEvent="submitHandler" name="userform" event="userEditEvent">
 
                     <text-input
@@ -46,93 +46,105 @@
                         label="Password"
                         :value="user.password"
                         name="password"></text-input>
-                        <hr>
-                        <div class="float-start">
-                         <input type="submit" class="btn btn-primary me-2" value="Save">
-                         <router-link to="/admin/users" class="btn btn-outline-secondary" >Cancel</router-link>
-                        </div>
-                        <div class="float-end">
-                            <a v-if="(this.$route.params.userId > 0) && (parseInt(String(this.$route.params.userId), 10) !== store.user.id)"
-                            class="btn btn-danger" href="javascript:void(0);" @click="confirmDelete(this.user.id)">Delete</a>
-                        </div>
-                        <div class="clearfix"></div>
 
-                        </form-tag>
-      </div>
+                    <hr>
+                    <div class="float-start">
+                        <input type="submit" class="btn btn-primary me-2" value="Save">
+                        <router-link to="/admin/users" class="btn btn-outline-secondary">Cancel</router-link>
+                    </div>
+                    <div class="float-end">
+                        <a v-if="(this.$route.params.userId > 0) && (parseInt(String(this.$route.params.userId), 10) !== store.user.id)"
+                            class="btn btn-danger" href="javascript:void(0);" @click="confirmDelete(this.user.id)">Delete</a>
+                    </div>
+                    <div class="clearfix"></div>
+
+                </form-tag>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-
 import Security from './security.js'
 import FormTag from './forms/FormTag.vue'
 import TextInput from './forms/TextInput.vue'
 import notie from 'notie'
-import router from './../router/index.js'
 import {store} from './store'
 
 export default {
-  beforeMount() {
-    Security.requireToken();
- 
-    if(parseInt(String(this.$route.params.userId), 10) > 0) { // based 10
-     // editing an existing user
-     // TODO - get user from db
+    beforeMount() {
+        Security.requireToken();
 
-    } 
-  },  
-   data(){
-    return {
-      user: {
-        id: 0,
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: "",
-      },
-      store,
-    }
-   },
-   components: {
-    'fomr-tag': FormTag,
-    'text-input': TextInput,
-
-   },
-   methods: {
-    submitHandler() {
-         const payload = {
-          id: parseInt(String(this.$route.params.userId), 10 ),
-          first_name: this.user.first_name,
-          last_name: this.user.last_name,
-          email: this.user.email,
-          password: this.user.password, 
-         }
-        fetch("http://localhost:8081/admin/users/save", Security.requestOptions(payload))
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.error) {
-            notie.alert({
-              type: 'error',
-              text: data.message,
+        if (parseInt(String(this.$route.params.userId), 10) > 0) {
+            // editing an existing user
+             // TODO - get user from db
+            fetch("http:localhost:8081/admin/users/get/" + this.$route.params.userId, Security.requestOptions(""))
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    notie.alert({
+                        type: 'error',
+                        text: data.message,
+                    })
+                } else {
+                    this.user = data;
+                    // we want password to be empty for existing users
+                    this.user.password = "";
+                }
             })
-          } else{
-            notie.alert({
-              type: 'success',
-              text: 'Changes saved!',
-            })
-          }
-        })
-        .catch((error)=>{
-          notie.alert({
-              type: 'error',
-              text: error,
-            })
-        })
+        }
     },
-    confirmDelete() {
+    data() {
+        return {
+            user: {
+                id: 0,
+                first_name: "",
+                last_name: "",
+                email: "",
+                password: "",
+            },
+            store,
+        }
+    },
+    components: {
+        'form-tag': FormTag,
+        'text-input': TextInput,
+    },
+    methods: {
+        submitHandler() {
+            const payload = {
+                id: parseInt(String(this.$route.params.userId), 10),
+                first_name: this.user.first_name,
+                last_name: this.user.last_name,
+                email: this.user.email,
+                password: this.user.password,
+            }
 
+            fetch(`${process.env.VUE_APP_API_URL}/admin/users/save`, Security.requestOptions(payload))
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    notie.alert({
+                        type: 'error',
+                        text: data.message,
+                    })
+                } else {
+                    notie.alert({
+                        type: 'success',
+                        text: 'Changes saved!',
+                    })
+                }
+            })
+            .catch((error) => {
+                notie.alert({
+                    type: 'error',
+                    text: error,
+                })
+            })
+        }, 
+        confirmDelete() {
+
+        }
     }
-   },
 }
 </script>
