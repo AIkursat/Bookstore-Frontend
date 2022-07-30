4f6b7c28078a0bc9e6eb815a7e9b99a3df8cc0fc
AIKursat <aynurkursat1999@hotmail.com>
2022-07-30 14:01:57 +0300

    Fetch ready status

--- vue-app/src/components/Users.vue
+++ vue-app/src/components/Users.vue
@@ -1,63 +1,66 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="row">
-        <h1 class="mt-3">All Users</h1>
-      </div>
-      <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-3">All Users</h1>
+            </div>
+            <hr>
 
-      <table class="table table-compact table-striped">
-        <thead>
-          <tr>
-            <th>User</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody> 
-          <tr v-for="u in this.users" v-bind:key="u.id">
-            <td>
-              <router-link :to="`/admin/users/${u.id}`">{{u.last_name}}, {{u.first_name}}</router-link>
-            </td>
-             <td>{{u.email}}</td>
-          </tr>
-        </tbody>
-      </table>
+            <table v-if="this.ready" class="table table-compact table-striped">
+                <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="u in this.users" v-bind:key="u.id">
+                        <td>
+                            <router-link :to="`/admin/users/${u.id}`">{{u.last_name}}, {{u.first_name}}</router-link>
+                        </td>
+                        <td>{{u.email}}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <p v-else>Loading...</p>
+
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import Security from './security.js'
-import notie from 'notie'
 
-export default{
-  data() {
-    return{
-      users: [],
-    }
-  },
-  beforeMount() {
-    Security.requireToken();
-
-    fetch("http://localhost:8081/admin/users", Security.requestOptions(""))
-    .then((response) => response.json())
-    .then((response) =>{
-      if(response.error){
-         notie.alert({
-          type: "error:",
-          text: response.message,
-         })
-      }else{
-        this.users = response.data.users;
-      }
-    })
-    .catch((error) => {
-          notie.alert({
-          type: "error:",
-          text: error,
-         })
-    });
-  },
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+export default {
+    data() {
+        return {
+            users: [],
+            ready: false,
+        }
+    },
+    beforeMount() {
+        Security.requireToken();
+
+        fetch("http://8081/admin/users", Security.requestOptions(""))
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.error) {
+                this.$emit('error', response.message);
+            } else {
+                sleep(3000).then(()=> {
+                    this.users = response.data.users;
+                    this.ready = true;
+                });
+            }
+        })
+        .catch((error) => {
+            this.$emit('error', error);
+        });
+    }
+}
 </script>
